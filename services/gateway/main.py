965c2f86983698fea4c7946fd561a7bda1f0e3f5
Matthias Green <matthiasgreen2@gmail.com>
2025-11-15 12:48:34 +0100

    Docker compose set up

--- services/gateway/main.py
+++ services/gateway/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from gateway service!"}
+    async with httpx.AsyncClient() as client:
+        r = await client.get("http://weather-data-service:8000")
+        return {"message": "Hello from gateway service!", "weather": r.json()}
 
