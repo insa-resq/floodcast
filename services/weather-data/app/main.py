0911fd97e0b55386539ad4dc7143574fbfb71389
Matthias Green <matthiasgreen2@gmail.com>
2025-11-18 14:51:58 +0100

    Add availability endpoint and pydantic data model

--- services/weather-data/app/main.py
+++ services/weather-data/app/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI, HTTPException
+from datetime import datetime
+from typing import Annotated
+
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import FileResponse
+<<<<<<< HEAD
 from datetime import datetime, timedelta
 from app.fetch import fetch_rainfall_local, UnavailableData
 from app.dependencies.config import Config
+||||||| parent of 433c495 (Add availability endpoint and pydantic data model)
+from datetime import datetime, timedelta
+from app.fetch import fetch_rainfall_local, UnavailableData
+=======
+
+from app.fetch import UnavailableData, fetch_rainfall_local, fetch_rainfall_availability_local
+from app.models import AvailabilityPeriod
+>>>>>>> 433c495 (Add availability endpoint and pydantic data model)
 
 app = FastAPI()
 
@@ -13,6 +25,14 @@ async def root():
         "message": "Hello from weather-data service!",
         "config": config
     }
+
+
+@app.get("/availability", response_model=list[AvailabilityPeriod])
+async def get_availability():
+    """
+    Get the list of available rainfall periods.
+    """
+    return list(fetch_rainfall_availability_local())
 
 
 @app.get(
@@ -31,17 +51,14 @@ async def root():
         },
     },
 )
-async def get_rainfall(
-    time: datetime = datetime.now(),
-    span: timedelta = timedelta(hours=1),
-):
+async def get_rainfall(availability: Annotated[AvailabilityPeriod, Query()]):
     """
     Get a TIFF format rainfall map. Unit: ??.
 
     Currently only accepts past dates if downloaded and a one hour span (using comephores).
     """
     try:
-        path = fetch_rainfall_local(time, span)
+        path = fetch_rainfall_local(availability)
         return FileResponse(path, media_type="image/tiff")
 
     except UnavailableData:
