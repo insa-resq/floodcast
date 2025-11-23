fd0e2482567f872011db2dbcd501f20ff54353e1
bahlous59 <medaminehassouna@gmail.com>
2025-11-23 21:17:44 +0100

    faire l'architecture de projet

--- services/weather-data/app/main.py
+++ services/weather-data/app/main.py
@@ -1,8 +1,22 @@
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
 
+async def get_config():
+    async with httpx.AsyncClient() as client:
+        r = await client.get("http://config-server:8000/config/weather-data")
+        return r.json()["config"]
+
+config = None
+
+@app.on_event("startup")
+async def load_config():
+    global config
+    config = await get_config()
+    print("Loaded config:", config)
+
 @app.get("/")
 async def root():
-    return {"message": "Hello from weather-data service!"}
+    return {"message": "weather data OK", "config": config}
 
