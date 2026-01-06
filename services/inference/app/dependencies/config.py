ff829dc0705d6f786ba5f77a33e5354f740b35be
Matthias Green <matthiasgreen2@gmail.com>
2026-01-06 10:09:04 +0100

    Scaffold inference service

---
+++ services/inference/app/dependencies/config.py
@@ -0,0 +1,27 @@
+from typing import Annotated, Literal
+
+from async_lru import alru_cache
+from fastapi import Depends
+from httpx import AsyncClient
+from pydantic import BaseModel
+
+
+class ConfigModel(BaseModel):
+    log_level: Literal["debug"]
+
+
+@alru_cache(maxsize=32)
+async def fetch_config() -> ConfigModel:
+    url = "http://config-service:8000/config/inference"
+    async with AsyncClient() as client:
+        r = await client.get(url)
+        r.raise_for_status()
+        data = r.json()
+        return ConfigModel(**data["config"])
+
+
+async def get_config() -> ConfigModel:
+    return await fetch_config()
+
+
+Config = Annotated[ConfigModel, Depends(get_config)]
