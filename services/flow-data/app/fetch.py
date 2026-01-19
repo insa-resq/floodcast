07728de1a4c9d0e6efa5143647fe2e266b85b89b
Matthias Green <matthiasgreen2@gmail.com>
2026-01-19 11:37:26 +0100

    Change endpoint for flow data for live readings

--- services/flow-data/app/fetch.py
+++ services/flow-data/app/fetch.py
@@ -1,8 +1,8 @@
-from datetime import date
+from datetime import date, datetime
 from typing import Literal
 
 from httpx import AsyncClient
-from pydantic import BaseModel, Field, OnErrorOmit, model_serializer, model_validator
+from pydantic import BaseModel, Field, OnErrorOmit, model_validator
 
 BASE_URL = "https://hubeau.eaufrance.fr/api/v2/hydrometrie"
 
@@ -47,38 +47,24 @@ class FlowQueryParams(BaseModel):
     longitude: float
     max_distance: int = Field(serialization_alias="distance")
     # site_code: str = Field(serialization_alias="code_entite")
-    start_date: date | None = Field(
-        default=None, serialization_alias="date_debut_obs_elab"
+    start_date: datetime | None = Field(
+        default=None, serialization_alias="date_debut_obs"
     )
-    end_date: date | None = Field(default=None, serialization_alias="date_fin_obs_elab")
+    end_date: datetime | None = Field(default=None, serialization_alias="date_fin_obs")
+    measure: Literal["Q", "H"] = Field(
+        default="Q", serialization_alias="grandeur_hydro"
+    )
 
 
 class FlowInfo(BaseModel):
     site_info: SiteInfo
-    obs_date: date = Field(validation_alias="date_obs_elab")
-    measure: Literal["Q", "H"]
-    measure_type: Literal["average", "min", "max"]
-    span: Literal["daily", "monthly"]
-    value: float = Field(validation_alias="resultat_obs_elab")
+    obs_date: datetime = Field(validation_alias="date_obs")
+    measure: Literal["Q", "H"] = Field(validation_alias="grandeur_hydro")
+    value: float = Field(validation_alias="resultat_obs")
 
     @model_validator(mode="before")
     @classmethod
     def decompose_measure(cls, data: dict):
-        measure_raw = data.get("grandeur_hydro_elab")
-        if measure_raw:
-            data["measure"] = measure_raw[0]
-            if data["measure"] == "Q":
-                data["resultat_obs_elab"] /= 60
-            else:
-                data["resultat_obs_elab"] /= 100
-            if "ix" in measure_raw.lower():
-                data["measure_type"] = "max"
-            elif "in" in measure_raw.lower():
-                data["measure_type"] = "min"
-            else:
-                data["measure_type"] = "average"
-            data["span"] = "daily" if "nj" in measure_raw.lower() else "monthly"
-
         if any(x in data for x in ["code_site", "longitude", "latitude"]):
             data = data.copy()
             data["site_info"] = {
@@ -91,30 +77,26 @@ class FlowInfo(BaseModel):
 
 
 class FlowResponse(BaseModel):
-    data: list[OnErrorOmit[FlowInfo]]
-    # data: list[FlowInfo]
+    # data: list[OnErrorOmit[FlowInfo]]
+    data: list[FlowInfo]
 
 
 async def fetch_flows(query: FlowQueryParams) -> FlowResponse:
     async with AsyncClient() as client:
         r = await client.get(
-            BASE_URL + "/obs_elab",
+            BASE_URL + "/observations_tr",
             params=query.model_dump(by_alias=True, exclude_none=True),
         )
-    stations = FlowResponse.model_validate_json(r.text)
+    stations = FlowResponse.model_validate_json(r.text, by_alias=True)
     return stations
 
 
 def latest_measure(
     flows: FlowResponse,
     measure: Literal["Q", "H"],
-    measure_type: Literal["average", "min", "max"],
-    span: Literal["daily", "monthly"],
 ) -> FlowInfo | None:
     filtered = filter(
-        lambda f: f.measure == measure
-        and f.measure_type == measure_type
-        and f.span == span,
+        lambda f: f.measure == measure,
         flows.data,
     )
     try:
@@ -132,9 +114,14 @@ if __name__ == "__main__":
                 latitude=43.604652,
                 longitude=1.444209,
                 max_distance=10,
-                start_date=date(year=2026, month=1, day=1),
+                start_date=datetime(year=2026, month=1, day=1),
             )
         )
-        print(latest_measure(flows, measure="H", measure_type="max", span="daily"))
+        print(
+            latest_measure(
+                flows,
+                measure="Q",
+            )
+        )
 
     asyncio.run(test())
